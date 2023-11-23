b68bac35d42a68f9b254c020d133646bbd64e71a
Artem Mkrtchyan <artemmkrtchyan@MacBook-Pro-Artem.local>
2023-11-23 09:19:56 +0300

    add stylelint to the project

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": 'stylelint-config-recommended-scss',
+    "plugins": [
+      'stylelint-selector-bem-pattern'
+    ],
+    "rules": {
+      'plugin/selector-bem-pattern': {
+        "preset": 'bem',
+        "componentSelectors": "bemSelector"
+      }
+    }
+  }
